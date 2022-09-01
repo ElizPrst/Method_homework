0cc1b4cf95be19c46e5ac761bc90994deb132619
PopovaElis <elizprest@gmail.com>
2022-09-01 14:24:13 +0500

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-// Task 1
+
 public class Main {
 
     public static int methodRandomYears(){
+        // Task 1
 
         java.util.Random random = new java.util.Random();
         int min = 1800;
@@ -10,35 +11,7 @@ public class Main {
         int randomYears = random.nextInt(diff)+ min;
         return randomYears;
     }
-
-
-    public static void main (String[] args){
-        int randomYears= methodRandomYears();
-
-        if ( (randomYears % 4 ==0) && randomYears % 100!=0)
-        {
-            System.out.println(randomYears+ " год является високосным");}
-        else if ( (randomYears % 4 ==0) && randomYears % 100!=0&& (randomYears % 400 == 0))
-        {
-            System.out.println(randomYears+ " год является високосным");}
-        else
-            System.out.println(randomYears+" год не является високосным");
-
-        //Task 2
-    int methodRandomOs =methodRandomOs();
-    int randomYearsPhone = methodRandomYearsPhone();
-        if (methodRandomOs == 1 && randomYearsPhone> 2015)
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        if (methodRandomOs== 1 && randomYearsPhone < 2015)
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        else {
-            if (methodRandomOs == 0 && randomYearsPhone > 2015)
-                System.out.println("Установите версию приложения для Android по ссылке");
-            if (methodRandomOs == 0 && randomYearsPhone < 2015)
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        }
-    }
-
+    // Task 2
     public static int methodRandomYearsPhone(){
 
         java.util.Random random = new java.util.Random();
@@ -48,13 +21,69 @@ public class Main {
         int randomYearsPhone = random.nextInt(diff)+ min;
         return randomYearsPhone;
     }
-        public static int methodRandomOs(){
+    public static int methodRandomOs(){
 
-            java.util.Random random = new java.util.Random();
-            int min = 1;
-            int max = 3;
-            int diff = max - min;
-            int methodRandomOs = random.nextInt(diff)+ min;
-            return methodRandomOs;
+        java.util.Random random = new java.util.Random();
+        int min = 1;
+        int max = 2;
+        int diff = max - min;
+        int methodRandomOs = random.nextInt(diff)+ min;
+        return methodRandomOs;
     }
-}
+    // Task 3
+    public static int methodDeliveryDistance(){
+        java.util.Random random = new java.util.Random();
+        int min = 1;
+        int max = 65;
+        int diff = max - min;
+        int methodDeliveryDistance = random.nextInt(diff)+ min;
+        return methodDeliveryDistance;
+    }
+
+    //у меня не получилось сделать выводы в разных методах, поэтому немного вперемешку задания =(
+
+    public static void main (String[] args){
+        System.out.println("Task 1");
+
+        { int randomYears= methodRandomYears();
+
+        if ( (randomYears % 4 ==0) && randomYears % 100!=0)
+        {
+            System.out.println(randomYears+ " год является високосным");}
+        else if ( (randomYears % 4 ==0) && randomYears % 100!=0&& (randomYears % 400 == 0))
+        {
+            System.out.println(randomYears+ " год является високосным");}
+        else
+            System.out.println(randomYears+" год не является високосным");}
+
+        System.out.println("Task 2");
+
+        {int methodRandomOs =methodRandomOs();
+    int randomYearsPhone = methodRandomYearsPhone();
+
+        if (methodRandomOs == 1 && randomYearsPhone> 2015)
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        if (methodRandomOs== 1 && randomYearsPhone < 2015)
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        else
+            if (methodRandomOs == 0 && randomYearsPhone > 2015)
+                System.out.println("Установите версию приложения для Android по ссылке");
+            if (methodRandomOs == 0 && randomYearsPhone < 2015)
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+
+        }
+            System.out.println("Task 3");
+
+            {int deliveryDistance = methodDeliveryDistance();
+            int deliveryDays = 1;
+
+                if ( deliveryDistance > 20)
+                    deliveryDays++;
+                    if ( deliveryDistance > 60)
+                        deliveryDays++;
+                        System.out.println("Потребуется дней: "+ deliveryDays );
+                    }
+                }
+            }
+
+
