0d24629a3b912fc00283e0400e35f2e01db08ab6
PopovaElis <elizprest@gmail.com>
2022-09-03 13:15:57 +0500

    Переделанная версия v2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 
 public class Main {
+    public static void main (String[] args){
+        System.out.println("Task 1");
+        methodLeapYear(methodRandomYears());
+
+
+        System.out.println ("Task 2");
+        methodDriverOs(methodRandomOs(),methodRandomYearsPhone());
+
+
+        System.out.println ("Task 3");
+        printDelivery(methodDeliveryDistance());
+    }
+
 
     // Task 1
     public static int methodRandomYears(){
@@ -11,11 +24,13 @@ public class Main {
             int randomYears = random.nextInt(diff)+ min;
             return randomYears;
     }
-    public  static boolean methodLeapYear(){
+    public  static void methodLeapYear(int randomYears){
 
-        int randomYears= methodRandomYears();
-        return (randomYears % 4 ==0) && randomYears % 100!=0 || (randomYears % 400 == 0);
-    }
+            if ( (randomYears % 4 ==0) && randomYears % 100!=0 || (randomYears % 400 == 0)==true)
+            { System.out.println( randomYears +" год является високосным");}
+            else
+            { System.out.println(randomYears +" год не является високосным");}
+        }
     // Task 2
     public static int methodRandomYearsPhone(){
 
@@ -25,6 +40,7 @@ public class Main {
         int diff = max - min;
         int randomYearsPhone = random.nextInt(diff)+ min;
         return randomYearsPhone;
+
     }
     public static int methodRandomOs(){
 
@@ -35,23 +51,14 @@ public class Main {
         int methodRandomOs = random.nextInt(diff)+ min;
         return methodRandomOs;
     }
-        public static void printOs() {
+    public static void methodDriverOs( int methodRandomOs,int randomYearsPhone){
 
-            int methodRandomOs =methodRandomOs();
-            int randomYearsPhone = methodRandomYearsPhone();
+    String version =randomYearsPhone < 2015 ? "облегченную":"";
+    String versionOs = methodRandomOs== 0 ? "OS":"Android";
 
-            if (methodRandomOs == 1 && randomYearsPhone> 2015)
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            if (methodRandomOs== 1 && randomYearsPhone < 2015)
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            else
-            if (methodRandomOs == 0 && randomYearsPhone > 2015)
-                System.out.println("Установите версию приложения для Android по ссылке");
-            if (methodRandomOs == 0 && randomYearsPhone < 2015)
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-
-        }
-
+        System.out.printf ("Установите %s версию приложения для %s по ссылке ",version,versionOs);
+        System.out.println();
+    }
     // Task 3
     public static int methodDeliveryDistance(){
         java.util.Random random = new java.util.Random();
@@ -61,36 +68,14 @@ public class Main {
         int methodDeliveryDistance = random.nextInt(diff)+ min;
         return methodDeliveryDistance;
     }
-    public static void printDelivery() {
-    int deliveryDistance = methodDeliveryDistance();
+    public static void printDelivery(int deliveryDistance) {
     int deliveryDays = 1;
 
-    if ( deliveryDistance > 20)
-    deliveryDays++;
-
-    if ( deliveryDistance > 60)
-    deliveryDays++;
-                    System.out.println("Потребуется дней: "+ deliveryDays );
+    if ( deliveryDistance > 60){deliveryDays =(Math.abs(deliveryDistance-60)+2);
     }
-
-    public static void main (String[] args){
-        System.out.println("Task 1");
-
-        if ( methodLeapYear()==true)
-        { System.out.println(methodRandomYears()+ " год является високосным");}
-            else
-        { System.out.println(methodRandomYears()+" год не является високосным");}
-
-    System.out.println ("Task 2");
-
-    printOs();
+                    System.out.println("Потребуется дней: "+ deliveryDays );
 
 
-    System.out.println ("Task 3");
-
-    printDelivery();
-
-
-                    }
-                }
+}
+}
 
